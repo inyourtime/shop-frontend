565040e19b82068dd82daa8bacebd33940298747
sahachai.t <sahachai@edvisory.co.th>
2023-09-29 09:19:31 +0700

    adas

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>About page</h1>
+  <div>Count: {{ count }}</div>
+  <button @click="increment">Click</button>
+</template>
+<script setup lang="ts">
+import { ref } from "vue"
+const count = ref(0);
+const increment = () => count.value++;
+</script>
