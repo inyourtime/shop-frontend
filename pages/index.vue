aaffa8b70afec6bb2218752f1ccf46bb00095403
sahachai.t <sahachai@edvisory.co.th>
2023-09-29 16:13:47 +0700

    add

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,11 @@
   <div>This is index page
     <h1>Hello this from CI</h1>
     <h1 style="color: red;">Hello this CD ja</h1>
+    <h1>Ladder≈</h1>
+    <h2>Ladder≈</h2>
+    <h3>Ladder≈</h3>
+    <h4>Ladder≈</h4>
+    <h5>Ladder≈</h5>
+    <h6>Ladder≈</h6>
   </div>
 </template>
