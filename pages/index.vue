262ab03477ea944ffdd36ce8dd8b2c0e57b0dbb9
sahachai.t <sahachai@edvisory.co.th>
2023-09-29 18:13:07 +0700

    add image

--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,28 @@
     <h4>Ladder≈</h4>
     <h5>Ladder≈</h5>
     <h6>Ladder≈</h6>
+    <ol>
+      <li>ONE</li>
+      <li>TWO</li>
+      <li>THREE</li>
+      <li>FOUR</li>
+      <li>FIVE</li>
+      <li>SIX</li>
+    </ol>
+    <!-- <div class="center"> -->
+    <img class="center"
+      src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg" alt="cat"
+      width="200px" height="200px">
+    <!-- </div> -->
+
   </div>
 </template>
+
+<style>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  /* width: 50%; */
+}
+</style>
